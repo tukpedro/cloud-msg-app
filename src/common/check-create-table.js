cc7f583c3a429b5bb1347800439d70f107d3830a
Pedro Pastuk <pastukpedro@gmail.com>
2023-12-11 01:45:09 -0300

    :wrench: helpers

---
+++ src/common/check-create-table.js
@@ -0,0 +1,37 @@
+const { DynamoDBClient, CreateTableCommand, ListTablesCommand } = require('@aws-sdk/client-dynamodb');
+const { region } = require('../config/index');
+
+const dynamoClient = new DynamoDBClient({ region });
+
+module.exports.checkCreateTable = async (tableName, attribiteName) => {
+
+    const data = await dynamoClient.send(new ListTablesCommand({}));
+    const existingTables = data.TableNames;
+
+    if (existingTables.includes(tableName)) {
+        return;
+    }
+
+    const params = {
+        TableName: tableName,
+        KeySchema: [
+            { AttributeName: attribiteName, KeyType: "HASH" },
+        ],
+        AttributeDefinitions: [
+            { AttributeName: attribiteName, AttributeType: "S" },
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+        }
+    };
+
+    try {
+        const results = await dynamoClient.send(new CreateTableCommand(params));
+        console.log("Tabela de usuários criada:", results);
+        return results;
+    } catch (error) {
+        console.error("Não foi possível criar a tabela:", error);
+    }
+}
+
---
+++ src/common/return-response.helper.js
@@ -0,0 +1,11 @@
+module.exports = (result, statusCode) => {
+	return {
+		statusCode,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+			'Access-Control-Allow-Headers': 'X-API-KEY',
+		},
+		body: JSON.stringify(result),
+	};
+};
